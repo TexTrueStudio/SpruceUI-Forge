e632a5a396e406e207fdd3b4579cdee97c147d66
TexTrue <65154269+TexBlock@users.noreply.github.com>
2022-10-02 23:38:22 +0800

    port to forge

---
+++ src/main/java/dev/lambdaurora/spruceui/SpruceUI.java
@@ -0,0 +1,40 @@
+package dev.lambdaurora.spruceui;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fmllegacy.network.FMLNetworkConstants;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(SpruceUI.MODID)
+public class SpruceUI {
+    public static final String MODID = "spruceui";
+    public static Logger LOGGER = LogManager.getLogger("SpruceUI-Forge");
+
+    public SpruceUI() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        MinecraftForge.EVENT_BUS.register(this);
+
+        ModLoadingContext.get()
+                .registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+    public static Logger logger() {
+        if (LOGGER == null) {
+            LOGGER = LogManager.getLogger("SpruceUI-Forge");
+        }
+
+        return LOGGER;
+    }
+
+}
